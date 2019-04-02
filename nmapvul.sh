b2c880bdba8b40156197a3f8bd8202ddb59f9ba5
System Administrator <root@MacBook-Pro-de-Erick.local>
2019-04-02 13:42:03 -0300

    melhoria na gravação de arquivo no nmap

--- nmapvul.sh
+++ nmapvul.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# nmapvul.sh
+# Criação: 23/03/2019
+# Propósito: Fazer teste de vulnerabilidade do Nmap
+# Autor: Erick Formaggio
 
 clear
 
@@ -7,4 +11,4 @@ echo "NMAP VULNERABILIDADES"
 echo "=================================================="
 echo "Analisando... O teste pode demorar alguns minutos"
 echo "Se quiser acompanhar os detalhes, em outra aba digite: tcpdump host ip-do-site -v"
-nmap -Pn --script vuln "$host" -v -oS  Testes/"$host"/"$host"_nmap-vulnerabilidades-$DATE.txt
+nmap -Pn --script vuln "$host" -v -oN  Testes/"$host"/"$host"_nmap-vulnerabilidades-$DATE.txt
